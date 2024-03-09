57cd28226fa7a7799ec34b94cd2984211ac47350
lemonprogis <edward.briggler@gmail.com>
2024-03-08 22:00:05 -0600

    bare bones start

---
+++ src/main/java/com/lemonprogis/zarfdemo/ZarfdemoApplication.java
@@ -0,0 +1,13 @@
+package com.lemonprogis.zarfdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarfdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZarfdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lemonprogis/zarfdemo/ZarfdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemonprogis.zarfdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarfdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
